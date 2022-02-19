f8b07d2e90645819b8029ebe24f5b958de2870d7
aizaomar <w.omaraiza@gmail.com>
2022-02-19 20:43:07 +0600

    fixed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
